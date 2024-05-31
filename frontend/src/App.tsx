14846318fb7dc53eaa907465c1783e9b53c280f0
EdivanMasepa <edimasepa@gmail.com>
2024-05-31 16:21:41 -0300

    feat: estilização tela de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='divPrincipal'>
+        <div className='divLogin'>
+
+          <div className='divImgLogo'>
+            <h3 className='logo'>LOGO</h3>
+          </div>
+
+          <div className='divH1Login'>
+            <h2 className='h1Login'>LOGIN</h2>
+          </div>
+        
+          <div className='divInputs'>
+            <div className='divConteudoInputs'>
+              <div className='divInputLogin'>
+                <label className='labelLogin'>Login</label>
+                <input className='inputLogin' type='text' placeholder='E-mail, CPF ou Telefone'></input>
+              </div>
+
+              <div >
+                <label className='labelSenha'>Senha</label>
+                <input className='inputSenha' type='password' placeholder='Senha'></input>
+                <h5><a>Esqueci minha senha</a></h5>
+              </div>
+            </div>
+          </div>
+
+          <div className='divButtonEntrar'>
+            <button className='buttonEntrar' type='submit'>Entrar</button>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
