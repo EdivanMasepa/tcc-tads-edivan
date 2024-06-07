291155ded217caf139963ffc7ca3285b059b11d5
Edivan <edimasepa@gmail.com>
2024-06-07 15:31:45 -0300

    feat: tela de login + estilização

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,16 +16,18 @@ function App() {
         
           <div className='divInputs'>
             <div className='divConteudoInputs'>
+
               <div className='divInputLogin'>
                 <label className='labelLogin'>Login</label>
                 <input className='inputLogin' type='text' placeholder='E-mail, CPF ou Telefone'></input>
               </div>
 
-              <div >
+              <div className='divInputSenha'>
                 <label className='labelSenha'>Senha</label>
                 <input className='inputSenha' type='password' placeholder='Senha'></input>
-                <h5><a>Esqueci minha senha</a></h5>
+                <h5 className='h5Senha'><a>Esqueci minha senha</a></h5>
               </div>
+
             </div>
           </div>
 
@@ -34,6 +36,12 @@ function App() {
           </div>
 
         </div>
+
+        <div className='divButtonCadastrar'>
+          <label className='labelCadastrar'>Não possui uma conta?</label>
+          <button className='buttonCadastrar' type='submit'>Cadastre-se</button>
+        </div>
+
       </div>
     </>
   )
