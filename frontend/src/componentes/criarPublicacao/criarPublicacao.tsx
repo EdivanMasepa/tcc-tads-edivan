fa46e4b088f0fa9d902ef8cec75925d03bc45999
Edivan <edimasepa@gmail.com>
2025-06-25 23:35:43 -0300

    feat(front-end): componente criarPublicacao

---
+++ frontend/src/componentes/criarPublicacao/criarPublicacao.tsx
@@ -0,0 +1,12 @@
+import { forwardRef } from "react";
+import './criarPublicacao.css'
+
+const CriarPublicacao = forwardRef<HTMLDivElement>((props, ref) => {
+ return(
+        <div className='divFundo'>
+
+        </div>
+
+    )
+})
+export default CriarPublicacao;
