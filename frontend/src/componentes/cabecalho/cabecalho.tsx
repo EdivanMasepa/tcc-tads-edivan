e1a0a0c9f44fccc9263d4697662058e493f2f096
Edivan <edimasepa@gmail.com>
2025-06-28 01:45:49 -0300

    fix(front-end - cabecalho): regras de negocio durante eventos

--- frontend/src/componentes/cabecalho/cabecalho.tsx
+++ frontend/src/componentes/cabecalho/cabecalho.tsx
@@ -7,20 +7,19 @@ import { useNavigate } from 'react-router-dom';
 import CriarPublicacao from '../criarPublicacao/criarPublicacao';
 
 const Cabecalho: React.FC = () => {
-    
   const [abreMenu, setAbreMenu] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
-  const abrirMenu = () => {setAbreMenu(!abreMenu)}
-
-  const [abreCriarPublicacao, setCriarPublicacao] = useState(false);
-  const criarPublicacaoRef = useRef<HTMLDivElement>(null);
-  const abrirCriarPublicacao = () => {setCriarPublicacao(!abreCriarPublicacao)};
-
-  const navigate = useNavigate();
-  const pesquisar = () => {navigate('/pesquisa')};
+  const abrirMenu = () => { if (abreCriarPublicacao) return; setAbreMenu(!abreMenu)}
   const fecharMenu = () => {setAbreMenu(false)};
-  const fecharCriarPublicacao = () => {setAbreMenu(false)};
 
+  const [abreCriarPublicacao, setAbreCriarPublicacao] = useState(false);
+  const criarPublicacaoRef = useRef<HTMLDivElement>(null);
+  const abrirCriarPublicacao = () => {setAbreCriarPublicacao(!abreCriarPublicacao)};
+  const fecharCriarPublicacao = () => {setAbreCriarPublicacao(false)};
+  
+  const navigate = useNavigate();
+  const pesquisar = () => { if (abreCriarPublicacao) return; navigate('/pesquisa')};
+  
   const cliqueFora = (event: MouseEvent) => {
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
       fecharMenu();
@@ -28,7 +27,7 @@ const Cabecalho: React.FC = () => {
   };
 
   const cancelarCriarPublicacao = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+    if (criarPublicacaoRef.current && !criarPublicacaoRef.current.contains(event.target as Node)) {
       fecharCriarPublicacao();
     }
   };
@@ -47,7 +46,6 @@ const Cabecalho: React.FC = () => {
 
   return(
     <>
-    
       <div className="divCabecalho">
           <div className="divLogoCabecalho">
               <a href='/paginaInicial' className='aLogo'><h3 className='logo'>LOGO</h3></a>
@@ -60,19 +58,22 @@ const Cabecalho: React.FC = () => {
           </div>
 
           <div className="divCabecalhoPesquisa">
-              <button type="submit" className='buttonPesquisa' onClick={pesquisar} ><IoSearch className='iconePesquisa' />
+              <button type="submit" className='buttonPesquisa' onClick={pesquisar} >
+                <IoSearch className='iconePesquisa' />
               </button>
           </div>
 
           <div className="divCabecalhoMenu">
             <button className='buttonMenu' onClick={abrirMenu}>                
-                {abreMenu ? <IoClose className='iconeMenu'/> : <img src='./perfil.png' alt="menu" className='imgMenu'/>}
+                {abreMenu && !abreCriarPublicacao ? <IoClose className='iconeMenu'/> : <img src='./perfil.png' alt="menu" className='imgMenu'/>}
             </button> 
           </div>
           
       </div>
-      {abreMenu && <Menu ref={menuRef} />}  
-      {abreCriarPublicacao && <CriarPublicacao ref={criarPublicacaoRef}/>}
+      {abreMenu && !abreCriarPublicacao && <Menu ref={menuRef} />} 
+      {abreCriarPublicacao && (
+        <CriarPublicacao ref={criarPublicacaoRef} onCancelar={fecharCriarPublicacao}/>
+      )} 
     </>
   )
 }
