60820175fe6dc7ee7a5021a80e21b1b8fc30d62c
EdivanMasepa <edimasepa@gmail.com>
2025-07-09 17:50:32 -0300

    fix(front-end menu): alteração na estilização e estrutura do menu

--- frontend/src/componentes/menu/menu.tsx
+++ frontend/src/componentes/menu/menu.tsx
@@ -1,6 +1,9 @@
 import   { forwardRef } from 'react'
 import './menu.css'
 import Cookies from 'js-cookie'
+import { FiUser } from 'react-icons/fi'
+import { TbLogout } from 'react-icons/tb'
+import { IoKeyOutline } from 'react-icons/io5'
 
 const Menu = forwardRef<HTMLDivElement>((props, ref) => {
     const deslogar = ()=>{
@@ -9,15 +12,39 @@ const Menu = forwardRef<HTMLDivElement>((props, ref) => {
 
     return(  
         <>
-        <div className='divBackgroundMenu'></div>
-        <div ref={ref} className="divMenu">
-            <ul className="ulMenu">
-                <li className="liMenu"><a className='linkMenu' href="/perfil">Perfil</a></li>
-                <li className="liMenu"><a className='linkMenu' onClick={deslogar} href="/login">Sair</a></li>
-            </ul>
-        </div>
+            <div className='divBackgroundMenu'></div>
+
+            <div ref={ref} className="divMenu">
+                <div className='divPerfilMenu'>
+                    <img src='./perfil.png' alt="menu" className='imgUsuarioMenu'/>
+
+                    <div className='divNomeUsuarioMenu'>
+                        <p className="pNomeUsuarioMenu">Fulano da silva</p>
+                    </div>
+
+                </div>
+
+                <div className='divListaMenu'>
+                    <ul className="ulMenu">
+                        <a className='linkMenu' href="/perfil">
+                            <li className="liMenu"><FiUser className='iconeListaMenu'/> Perfil</li>
+                        </a>
+
+                        <a className='linkMenu' href="/redefinirSenha">
+                            <li className="liMenu"><IoKeyOutline className='iconeListaMenu'/> Alterar senha</li>
+                        </a>
+
+                        <a className='linkMenu' href="/login" onClick={deslogar}>
+                            <li className="liMenu"> <TbLogout className='iconeListaMenu'/> Sair</li>
+                        </a>
+                    </ul>
+                </div>
+
+                <div className='divRodapeMenu'>
+                    <a href='./paginaSobre' className='aSobre'>Sobre o sistema</a>
+                </div>
+            </div>
         </>          
-        
     )
 })
-export default Menu
+export default Menu;
