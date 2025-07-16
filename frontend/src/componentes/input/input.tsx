a6362b28c38b57625ad8ae3f3f4d1b80a20f7d60
EdivanMasepa <edimasepa@gmail.com>
2025-07-16 17:57:23 -0300

    fix(front-end input): regex em campos

--- frontend/src/componentes/input/input.tsx
+++ frontend/src/componentes/input/input.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './input.css'
 
 interface InputProps {
@@ -5,19 +6,64 @@ interface InputProps {
     placeholder?: string;
     type?:string
     value?: string;
-    setValue?: (value: string) => void
+    setValue?: (value: string) => void;
+    mask?: 'telefone' | 'cpf' | 'cnpj' | 'data' | string;
+    maxLength?: number
 }
 
-export default function Input({ ...props }: InputProps){
+const mascara = (valor: string, tipo: string): string => {
+    const digitos = valor.replace(/\D/g, '');
+
+    switch (tipo){
+        case 'telefone':
+            if(digitos.length <= 10)
+                 return digitos.replace(/^(\d{0,2})(\d{0,4})(\d{0,4})/, (_, a, b, c) =>
+                    [a && `(${a}`, b && `) ${b}`, c && `-${c}`].filter(Boolean).join('')
+                );
+            else
+                return digitos.replace(/^(\d{0,2})(\d{0,5})(\d{0,4})/, (_, a, b, c) =>
+                    [a && `(${a}`, b && `) ${b}`, c && `-${c}`].filter(Boolean).join('')
+                );
+            
+        case 'cpf':
+            return digitos.replace(/^(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/, (_, a, b, c, d) =>
+                [a, b, c].filter(Boolean).join('.') + (d ? `-${d}` : '')
+            );
+        case 'cnpj':
+            return digitos.replace(/^(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/, (_, a, b, c, d, e) =>
+                [a, b, c].filter(Boolean).join('.') + (d ? `/${d}` : '') + (e ? `-${e}` : '')
+            );
+        case 'data':
+            return digitos.replace(/^(\d{0,2})(\d{0,2})(\d{0,4})/, (_, d, m , y) => [d, m, y].filter(Boolean).join('/'));
+
+        default: 
+            return valor;
+    }
+}
+
+export default function Input({label, placeholder, type = 'text', value, setValue, mask = 'nenhuma', maxLength }: InputProps){
+    const aplicarMascara = (evento: React.ChangeEvent<HTMLInputElement>) => {
+        const mascaraValor = mascara(evento.target.value, mask);
+        setValue?.(mascaraValor)
+    }
     return(
         <div className='divPrincipalInput'>
+
             <div className='divLabel'>
-                <label className='label'>{props.label}</label>
+                <label className='label'>{label}</label>
             </div>
+
             <div className='divInput'>
-                <input className='input' onChange={(e) => {
-                    props.setValue ? props.setValue(e.target.value) : null
-                }} type={props.type} placeholder={props.placeholder} value={props.value}></input>            
+
+                <input 
+                    className='input'
+                    placeholder={placeholder}
+                    type={type}
+                    value={value}              
+                    onChange={aplicarMascara} 
+                    maxLength={maxLength} 
+                />  
+                          
             </div>
         </div>
     )
