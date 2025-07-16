e0f42ac1202375c69d0c271e27de08bfd59bd749
EdivanMasepa <edimasepa@gmail.com>
2025-07-16 17:56:46 -0300

    fix(front-end cadastro): valores predefinidos em campos

--- frontend/src/paginas/cadastro/cadastro.tsx
+++ frontend/src/paginas/cadastro/cadastro.tsx
@@ -9,6 +9,8 @@ import axios from 'axios';
 import  'react-toastify/dist/ReactToastify.css' ;
 import { RetornoRequisicao } from '../../types/retornoRequisicao';
 import { api } from '../../api';
+import SelectDemo from '../../componentes/select/select-radix';
+import { VscArchive } from 'react-icons/vsc';
 
 enum TipoCadastroEnum{
   PESSOA='Pessoa' ,
@@ -78,9 +80,8 @@ const Cadastro: React.FC = () => {
   const [situacao, setSituacao] = useState<string | null>(null);
   const listaCamposCadastro = [tipoCadastro, nome, email, telefone, senha, confirmaSenha, cpfOuCnpj, dataNascimentoOuFundacao, generoOuSegmento];
   const [opcaoCadastro, setOpcaoCadastro] = useState(true);
-  const alteraOpcaoCadastro = () =>{setOpcaoCadastro(!opcaoCadastro)};
+  const alteraOpcaoCadastro = () =>{setOpcaoCadastro(!opcaoCadastro);limparCampos();};
   let campoVazio: boolean = false
-  
   
   const dadosPessoa: UsuarioPessoaInterface | any = {
       cpf: cpfOuCnpj,
@@ -92,8 +93,7 @@ const Cadastro: React.FC = () => {
   const dadosInstituicao: UsuarioInstituicaoInterface | any = {
       cnpj: cpfOuCnpj,
       dataFundacao: dataNascimentoOuFundacao,
-      segmento: generoOuSegmento,
-      situacao: situacao
+      segmento: generoOuSegmento
   }
 
   const dados: DadosCadastroInterface | any = {
@@ -103,7 +103,18 @@ const Cadastro: React.FC = () => {
       telefone: telefone,
       senha: senha,
       confirmaSenha: confirmaSenha,
-      pessoa: dadosPessoa
+      pessoa: tipoCadastro === TipoCadastroEnum.PESSOA ? dadosPessoa : dadosInstituicao
+  }
+
+  const limparCampos = () => {
+      setNome(null);
+      setEmail(null);
+      setTelefone(null);
+      setSenha(null);
+      setDataNascimentoOuFundacao(null);
+      setCpfOuCnpj(null);
+      setGeneroOuSegmento(null);
+      setSituacao(null);
   }
   
   const cadastrar =  async (dadosCadastro: DadosCadastroInterface) => {
@@ -120,16 +131,9 @@ const Cadastro: React.FC = () => {
 
     try{
       const { data } = await api.post<RetornoRequisicao>('/usuario/cadastrar', dadosCadastro);
-      setNome(null);
-      setEmail(null);
-      setTelefone(null);
-      setSenha(null);
-      setDataNascimentoOuFundacao(null);
-      setCpfOuCnpj(null);
-      setGeneroOuSegmento(null);
-      setSituacao(null);
       toast.dismiss();
       toast.success(data.message || 'Cadastro realizado com sucesso!');
+      limparCampos();
     }catch (erro) {
       const msg = axios.isAxiosError(erro)
         ? erro.response?.data?.message || 'Erro ao cadastrar.'
@@ -143,6 +147,33 @@ const Cadastro: React.FC = () => {
       }, 100);
     }
   }
+  const opcoesGenero = [
+      { id : 1, label: GeneroPessoaEnum.MASCULINO , value: GeneroPessoaEnum.MASCULINO },
+      { id : 2, label: GeneroPessoaEnum.FEMININO, value: GeneroPessoaEnum.FEMININO }
+    ]
+
+  const opcoesSegmento = [
+    { id : 1, label: SegmentoInstituicaoEnum.ADMINISTRACAO_PUBLICA , value: SegmentoInstituicaoEnum.ADMINISTRACAO_PUBLICA },
+    { id : 2, label: SegmentoInstituicaoEnum.AGRICULTURA , value: SegmentoInstituicaoEnum.AGRICULTURA },
+    { id : 3, label: SegmentoInstituicaoEnum.ASSISTENCIA_SOCIAL , value: SegmentoInstituicaoEnum.ASSISTENCIA_SOCIAL },
+    { id : 4, label: SegmentoInstituicaoEnum.COMERCIO , value: SegmentoInstituicaoEnum.COMERCIO },
+    { id : 5, label: SegmentoInstituicaoEnum.COMUNICACAO_MIDIA , value: SegmentoInstituicaoEnum.COMUNICACAO_MIDIA },
+    { id : 6, label: SegmentoInstituicaoEnum.COOPERATIVISMO , value: SegmentoInstituicaoEnum.COOPERATIVISMO },
+    { id : 7, label: SegmentoInstituicaoEnum.CULTURA , value: SegmentoInstituicaoEnum.CULTURA },
+    { id : 8, label: SegmentoInstituicaoEnum.DEFESA_CIVIL , value: SegmentoInstituicaoEnum.DEFESA_CIVIL },
+    { id : 9, label: SegmentoInstituicaoEnum.EDUCACAO , value: SegmentoInstituicaoEnum.EDUCACAO },
+    { id : 10, label: SegmentoInstituicaoEnum.ESPORTE_LAZER , value: SegmentoInstituicaoEnum.ESPORTE_LAZER },
+    { id : 11, label: SegmentoInstituicaoEnum.FINANCAS , value: SegmentoInstituicaoEnum.FINANCAS },
+    { id : 12, label: SegmentoInstituicaoEnum.INDUSTRIA , value: SegmentoInstituicaoEnum.INDUSTRIA },
+    { id : 13, label: SegmentoInstituicaoEnum.ORGANIZACAO_NAO_GOVERNAMENTAL , value: SegmentoInstituicaoEnum.ORGANIZACAO_NAO_GOVERNAMENTAL },    { id : 10, label: SegmentoInstituicaoEnum.ESPORTE_LAZER , value: SegmentoInstituicaoEnum.ESPORTE_LAZER },
+    { id : 14, label: SegmentoInstituicaoEnum.ORGANIZACAO_RELIGIOSA , value: SegmentoInstituicaoEnum.ORGANIZACAO_RELIGIOSA },
+    { id : 15, label: SegmentoInstituicaoEnum.OUTROS , value: SegmentoInstituicaoEnum.OUTROS },
+    { id : 16, label: SegmentoInstituicaoEnum.SAUDE , value: SegmentoInstituicaoEnum.SAUDE },
+    { id : 17, label: SegmentoInstituicaoEnum.SEGURANCA_PUBLICA , value: SegmentoInstituicaoEnum.SEGURANCA_PUBLICA },
+    { id : 18, label: SegmentoInstituicaoEnum.TECNOLOGIA , value: SegmentoInstituicaoEnum.TECNOLOGIA },
+    { id : 19, label: SegmentoInstituicaoEnum.TRANSPORTE , value: SegmentoInstituicaoEnum.TRANSPORTE },            
+
+  ]
   return (
     <>
       <div className='divPrincipal alturaCadastroDivPrincipal'>
@@ -193,19 +224,50 @@ const Cadastro: React.FC = () => {
 
               <Input value={email ?? ""} setValue={setEmail} label='Email' placeholder='exemplo@email.com'  type='email'/>
 
-              <Input value={telefone ?? ""} setValue={setTelefone} label='Telefone' placeholder='(11) 91111-1111'  type='text'/>
+              <Input value={telefone ?? ""} setValue={setTelefone} label='Telefone' placeholder='(00) 90000-0000'  type='text' mask='telefone'/>
 
-              <Input value={cpfOuCnpj ?? ""} setValue={setCpfOuCnpj} label={opcaoCadastro ? 'CPF' : 'CNPJ'} placeholder={opcaoCadastro ? '123.456.789-10' : '12.345.687/0001-23'}  type='text'/>
+              {opcaoCadastro === true ? 
+                <Input value={cpfOuCnpj ?? ""} setValue={setCpfOuCnpj} label='CPF' placeholder='000.000.000-00' type='text' mask='cpf' maxLength={14}/> :
+                <Input value={cpfOuCnpj ?? ""} setValue={setCpfOuCnpj} label='CNPJ' placeholder='00.000.000/0000-00' type='text' mask='cnpj' maxLength={18}/>
+              }
 
-              <Input value={dataNascimentoOuFundacao ?? ""} setValue={setDataNascimentoOuFundacao} label={opcaoCadastro ? 'Data de nascimento': 'Data de fundação' } placeholder='01/01/0001'  type='text'/>
+              <Input value={dataNascimentoOuFundacao ?? ""} setValue={setDataNascimentoOuFundacao} label={opcaoCadastro ? 'Data de nascimento': 'Data de fundação' } placeholder='00/00/0000' type='text' mask='data' maxLength={10}/>
 
-              <Input value={generoOuSegmento ?? ""} setValue={setGeneroOuSegmento} label={opcaoCadastro ? 'Gênero': 'Área de atuação' } placeholder=''  type='text'/>
+              {/* <Input value={generoOuSegmento ?? ""} setValue={setGeneroOuSegmento} label={opcaoCadastro ? 'Gênero': 'Área de atuação' } placeholder=''  type='text'/> */}
 
-              {opcaoCadastro && <Input value={situacao ?? ""} setValue={setSituacao} label='Situação' placeholder=''  type='text'/>} 
+              <div className='divSelectCadastro'>
+                {opcaoCadastro === true ? 
+                  <>
+                    <label className='labelSelectCadastro'>Gênero</label>
+                    <SelectDemo 
+                      value={generoOuSegmento} 
+                      onValueChange={setGeneroOuSegmento}
+                      options={opcoesGenero.map( (opcao) => (
+                        { key : opcao.id, label: opcao.label , value: opcao.value }
+                      ))}
+                      className='selectCadastro'
+                    />
+                  </> 
+                  :
+                  <>
+                    <label className='labelSelectCadastro'>Segmento</label>
+                    <SelectDemo 
+                      value={generoOuSegmento} 
+                      onValueChange={setGeneroOuSegmento}
+                      options={opcoesSegmento.map( (opcao) => (
+                        { key : opcao.id, label: opcao.label , value: opcao.value }
+                      ))}
+                    />
+                  </>
+                }
+              </div>
+              
 
-              <Input value={senha ?? ""} setValue={setSenha} label='Senha' placeholder='••••••••'  type='text'/>
+              {opcaoCadastro && <Input value={situacao ?? ""} setValue={setSituacao} label='Situação' placeholder='' type='text'/>} 
 
-              <Input value={confirmaSenha ?? ""} setValue={setConfirmarSenha} label='Confirmar senha' placeholder='••••••••'  type='text'/>
+              <Input value={senha ?? ""} setValue={setSenha} label='Senha' placeholder='••••••••' type='text'/>
+
+              <Input value={confirmaSenha ?? ""} setValue={setConfirmarSenha} label='Confirmar senha' placeholder='••••••••' type='text'/>
 
               <Button legenda='Cadastrar' onClick={() => cadastrar(dados)}/>
             </div>
