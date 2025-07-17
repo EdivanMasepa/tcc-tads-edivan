cd1e74f7e7bf4f18b776408e5abfacc44ff8df8a
EdivanMasepa <edimasepa@gmail.com>
2025-07-17 16:10:25 -0300

    fix(front-end pesquisa): alteração da função de pesquisar

--- frontend/src/paginas/pesquisa/pesquisa.tsx
+++ frontend/src/paginas/pesquisa/pesquisa.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import "./pesquisa.css"
-import '../../index.css'
 import Button from "../../componentes/botao/botao";
 import SelectDemo from "../../componentes/select/select-radix";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie"
 import axios from 'axios'
 import { jwtDecode, JwtPayload } from 'jwt-decode'
+import { useEffect, useState } from "react";
+import "./pesquisa.css";
+import '../../index.css';
 import { IoClose, IoSearch } from "react-icons/io5";
 import { api } from "../../api";
 
@@ -14,65 +14,69 @@ enum CategoriaPublicacaoEnum{
     ajuda = 'Pedir ajuda' ,
     campanha = 'Promover campanha',
     informativa = 'informativa'
-}
+};
+
+interface ResultadoUsuarioInterface{
+    id: string;
+    nome: string;
+};
+
+interface ResultadoPublicacaoInterface{
+    id: string;
+    titulo: string;
+};
 
 interface ResultadoPesquisaInterface{
-    id: number,
-    categoria: CategoriaPublicacaoEnum,
-    titulo: string,
-    descricao: string,
-    data: string,
-    aprovada: boolean,
-    nomeUsuarioResponsavel: string
- }
-
+    usuarios?: ResultadoUsuarioInterface[];
+    publicacoes: ResultadoPublicacaoInterface[];
+};
 
 const Pesquisa: React.FC = () => {
-    const [valor, setValor] = useState("");
-    const [resultados, setResultados] = useState<ResultadoPesquisaInterface[]>([]);
-
+    const [valor, setValor] = useState('');
+    const [resultados, setResultados] = useState<ResultadoPesquisaInterface | null>(null);
     const [tipoPublicacao, setTipoPublicacao] = useState<CategoriaPublicacaoEnum>(CategoriaPublicacaoEnum.informativa);
     const [opcao, setOpcao] = useState<number | null>(0);
-    const alteraOpcaoPublicacao = (buttonSelecionado: number) => {
-        setOpcao(buttonSelecionado)
-    };
+    const alteraOpcaoPublicacao = (buttonSelecionado: number) => {setOpcao(buttonSelecionado)};
+
     const buttons = [
         { id: 0, legenda: 'Pedir ajuda', boxShadow: 'shadowDireita', value:CategoriaPublicacaoEnum.ajuda, setValue:setTipoPublicacao },
         { id: 1, legenda: 'Promover campanha', boxShadow: 'shadowDuplo', value:CategoriaPublicacaoEnum.campanha, setValue:setTipoPublicacao },
         { id: 2, legenda: 'Informativo', boxShadow: 'shadowEsquerda', value:CategoriaPublicacaoEnum.informativa, setValue:setTipoPublicacao },
-      ];
-
+    ];
 
     useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (valor.trim() !== "") {
-        pesquisar(valor);
-      } else {
-        setResultados([]); 
-      }
+        const timeout = setTimeout(() => {
+            if (valor.trim() !== "") {
+                pesquisar(valor);
+            } else {
+                setResultados(null); 
+            }
         }, 500); 
 
         return () => clearTimeout(timeout); 
     }, [valor]);
     
     const pesquisar = async (texto: string) => {
-    try {
-      const response = await api.get(`publicacao/buscar-por-texto/${encodeURIComponent(texto)}`);
-      setResultados(response.data); console.log(response.data)
-    } catch (error) {
-      console.error("Erro ao buscar publicações:", error);
-    }
-  };
+        try {
+            const { data } = await api.get<ResultadoPesquisaInterface>(`usuario/buscar-por-texto/${encodeURIComponent(texto)}`);
+            setResultados(data);
+        } catch (error) {
+            console.error("Erro ao buscar publicações:", error);
+        }
+    };
+
+    const listaPublicacoes = resultados?.publicacoes != null && resultados.publicacoes.length > 0 ? resultados?.publicacoes : []
+    const listaUsuarios = resultados?.usuarios != null && resultados.usuarios.length > 0 ? resultados?.usuarios : []
 
     return(
         <>
             <div className='divPesquisa'>
-                <div className="divPaginaCabecalhoPesquisa">
+                <div className='divPaginaCabecalhoPesquisa'>
                     <div className='divIconePesquisa'> 
                         <IoSearch className='iconePesquisa iconePesquisaBorda' />
                     </div>
 
-                    <div className='divInputPesquisa '> 
+                    <div className='divInputPesquisa'> 
                         <input
                             type='text'
                             className='inputPesquisa'
@@ -93,19 +97,33 @@ const Pesquisa: React.FC = () => {
                 </div>
 
                 <div className='divResultadosPesquisa'>
-                    {resultados.map((resultado) =>(
-                        <div className='divItemResultado' key={resultado.id}> 
-                            <div className='divItemResultadoPesquisa'>
-                                <p className='pItemResultado'>Usuario: </p> 
-                                <p className='pItemResultadoValor'>{resultado.nomeUsuarioResponsavel}</p>
-                            </div>
-                            {resultado.titulo && 
+                    {listaPublicacoes.map((publicacao) =>(
+                        <div className='divItemResultado' key={publicacao.id}> 
+                            {/* <div className='divItemResultadoPesquisa'> */}
+                                <p className='pItemResultado'>Publicação: </p> 
+                                <p className='pItemResultadoValor'>{publicacao.titulo}</p>
+                            {/* </div> */}
+                            {/* {resultado.titulo && 
                                 <div className='divItemResultadoPesquisa'>
                                     <p className='pItemResultado'>Publicação: </p> 
                                     <p className='pItemResultadoValor'>{resultado.titulo}</p>
                                 </div>
-                            }
-                        </div>
+                            } */}
+                        </div>                            
+                    ))} 
+                    {listaUsuarios.map((usuario) =>(
+                        <div className='divItemResultado' key={usuario.id}> 
+                            {/* <div className='divItemResultadoPesquisa'> */}
+                                <p className='pItemResultado'>Usuário: </p> 
+                                <p className='pItemResultadoValor'>{usuario.nome}</p>
+                            {/* </div> */}
+                            {/* {resultado.titulo && 
+                                <div className='divItemResultadoPesquisa'>
+                                    <p className='pItemResultado'>Publicação: </p> 
+                                    <p className='pItemResultadoValor'>{resultado.titulo}</p>
+                                </div>
+                            } */}
+                        </div>                            
                     ))}    
                 </div>
 
